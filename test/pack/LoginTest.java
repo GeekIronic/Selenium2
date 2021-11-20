bbc7601db9ea5bf3fa4d9e797cdbed8fc443ba86
GeekIronic <juanpablomahechap@gmail.com>
2021-11-20 10:27:27 -0500

    Mejora en la construcción de un caso de prueba usando alertas de js

--- test/pack/LoginTest.java
+++ test/pack/LoginTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -20,23 +21,49 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * @author JuanPablo
  */
 public class LoginTest {
-    
+
     private WebDriver driver;
-  
+    By registerLinkLocator = By.linkText("Sign up");
+    By usernameLocator = By.id("sign-username");
+    By passwordLocator = By.id("sign-password");
+    By btnRegistrar = By.xpath("//button[@onclick='register()']");
+    private String Mensaje = "";
+    //WebElement alert = driver.findElement(By.linkText("Aceptar"));
+
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "./test/driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com/");
-        
+        driver.get("https://www.demoblaze.com/");
+
     }
-    
+
     @Test
-    public void Test(){
+    public void TestRegistro() throws InterruptedException {
+        driver.findElement(registerLinkLocator).click();
+        Thread.sleep(2000);
+        driver.findElement(usernameLocator).sendKeys("");
+        driver.findElement(passwordLocator).sendKeys("");
+        driver.findElement(btnRegistrar).click();
+        Thread.sleep(2000);
+        Mensaje = driver.switchTo().alert().getText();
+        Thread.sleep(2000);
+        assertEquals("Please fill out Username and Password.", driver.switchTo().alert().getText());
+        //assertEquals("This user already exist.", driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
+        
+        //
+        //assertEquals("Sign up successful.", driver.switchTo().alert().getText());
+        /*}else if(Mensaje == ""){
+            System.out.println("Escenario de validación de campos");
+        }else{
+            System.out.println(Mensaje);
+        }
+         */
     }
-    
-    
+
     @After
     public void tearDown() {
     }
